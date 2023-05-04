be5123b6823f446f9bdd2dbc9ee7693cda58c729
rnogales0 <128685671+rnogales0@users.noreply.github.com>
2023-05-04 09:53:41 +0200

    login cambiado
    xd

--- src/trabajoFinSpace/src/application/Login.java
+++ src/trabajoFinSpace/src/application/Login.java
@@ -1,4 +1,4 @@
-package Paquete;
+package application;
 
 import java.awt.EventQueue;
 
@@ -10,9 +10,11 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import java.awt.Color;
 import javax.swing.border.TitledBorder;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.awt.Font;
 
-public class Log extends JFrame {
+public class Login extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
@@ -37,9 +39,10 @@ public class Log extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Log() {
+	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 700, 550);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(128, 128, 192));
 		contentPane.setBorder(new TitledBorder(null, "Iniciar sesi\u00F3n", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -48,25 +51,39 @@ public class Log extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Usuario:");
-		lblNewLabel.setBounds(85, 59, 62, 19);
+		lblNewLabel.setBounds(85, 45, 62, 19);
 		contentPane.add(lblNewLabel);
 		
 		textField = new JTextField();
-		textField.setBounds(183, 59, 96, 19);
+		textField.setBounds(183, 45, 96, 19);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Contraseña:");
-		lblNewLabel_1.setBounds(85, 127, 62, 13);
+		lblNewLabel_1.setBounds(85, 111, 62, 13);
 		contentPane.add(lblNewLabel_1);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(183, 124, 96, 19);
+		passwordField.setBounds(183, 108, 96, 19);
 		contentPane.add(passwordField);
 		
 		JButton btnNewButton = new JButton("Entrar");
-		btnNewButton.setBounds(136, 174, 85, 21);
+		btnNewButton.setBounds(157, 156, 85, 19);
 		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Registrate");
+		btnNewButton_1.setBounds(308, 202, 85, 21);
+		contentPane.add(btnNewButton_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("¿Todavía no estás registrado? Clic en el botón ->");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_2.setBounds(33, 206, 265, 13);
+		contentPane.add(lblNewLabel_2);
+	
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		lblNewLabel_3.setBounds(0, 0, 700, 550);
+		lblNewLabel_3.setIcon(new ImageIcon(new ImageIcon("src/imagenes/fondo_menu.jpg").getImage().getScaledInstance(644, 550, getDefaultCloseOperation())));
+		contentPane.add(lblNewLabel_3);
 	}
-
 }

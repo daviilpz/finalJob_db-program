46f63520c30a485287b5c01329b8260917cf725b
JesusAtieenza <jaa0014@alu.medac.es>
2023-05-05 09:08:29 +0200

    rank

---
+++ src/trabajoFinSpace/src/application/Ranking.java
@@ -0,0 +1,71 @@
+package application;
+
+import java.awt.EventQueue;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class Ranking extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ranking frame = new Ranking();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 * @throws SQLException 
+	 */
+	public Ranking() throws SQLException {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(59, 36, 314, 181);
+		contentPane.add(lblNewLabel);
+		
+		final String DB_URL = "jdbc:mysql://localhost/registro?serverTimezoneUTC";
+		final String USERNAME = "root";
+		final String PASS = "";
+		Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASS);
+		Statement stmt = conn.createStatement();
+		String sql = "SELECT usuario, score FROM /*Nombre tabla*/ ORDER BY score ASC";
+		ResultSet rs = stmt.executeQuery(sql);
+		if (rs.next()) {
+		    String usuario = rs.getString("usuario");
+		    String score = rs.getString("score");
+		    String texto = "usuario: " + usuario + ", score: " + score;
+		    lblNewLabel.setText(texto);
+		    rs.close();
+		    stmt.close();
+		    conn.close();
+		}
+		
+	}
+
+}
